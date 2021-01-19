71df67c885f3854f453c1b28c8fb03eefbf822c7
Nilber Remon <nilberremon@gmail.com>
2021-01-19 16:06:28 -0500

    TensorFlow Config

--- TensorFlowJSLinearRegression/sketch.js
+++ TensorFlowJSLinearRegression/sketch.js
@@ -1,16 +1,21 @@
 import p5 from 'p5';
+import tf from ''
 
 let xs = []
 let ys = []
+
+let m, b
 
 const sketch = (s) => {
     
     let width = 500
     let height = 500
+
     s.setup = () => {
         s.createCanvas(width, height);
-    }
 
+        m = tf.scalar(random(1))
+    }
 
     s.mousePressed = () => {
         
@@ -25,12 +30,15 @@ const sketch = (s) => {
         s.background(0);
         s.stroke(255);
         s.strokeWeight(8)
+        
+        
         for(let i=0; i < xs.length; i++){
             let px = s.map(xs[i], 0, 1, 0, width)
             let py = s.map(ys[i], 0, 1, 0, height)
             s.point(px,py)
         }
     }
+
 
 
 
